a1b829af9d9e8ddf6eecb6101561f30320b3d684
Alexandr <alpinefuchs@gmail.com>
2026-05-04 03:31:59 +0300

    feat: start Control Unit

--- machine.py
+++ machine.py
@@ -1,5 +1,5 @@
 from AppData.Local.Programs.Python.Python312.Lib import random
-from isa import from_bytes_instruction, from_bytes_data
+from isa import from_bytes_instruction, from_bytes_data, Instruction, Opcode
 from typing import List
 from enum import Enum, auto
 
@@ -31,12 +31,12 @@ class Mux_Signal(Enum):
     SEL_PC_RET = auto()
     SEL_PC_NEXT = auto()
     SEL_PC_ADDR = auto()
+    SEL_PC_JZ = auto()
+    SEL_PC_JN = auto()
 
-    SEL_RET_NEXT = auto()
-    SEL_RET_PREV = auto()
+    SEL_R_NEXT = auto()
+    SEL_R_PREV = auto()
 
-    JUMP_TYPE_N = auto()
-    JUMP_TYPE_Z = auto()
 
 class ALU_Signal(Enum):
 
@@ -49,12 +49,6 @@ class ALU_Signal(Enum):
 
 class Signal(Enum):
 
-    # Stack operations
-    DATA_STACK_PUSH = auto()
-    DATA_STACK_POP = auto()
-    RETURN_STACK_PUSH = auto()
-    RETURN_STACK_POP = auto()
-
     LATCH_SP = auto()
     LATCH_AR = auto()
     LATCH_S = auto()
@@ -64,9 +58,9 @@ class Signal(Enum):
     LATCH_PC = auto()
     LATCH_MPC = auto()
 
-
-
-    MEMORY_WRITE = auto()
+    WRITE_MEM = auto()
+    WRITE_ST = auto()
+    WRITE_RET = auto()
     OUTPUT = auto()
     INPUT = auto()
 
@@ -92,14 +86,16 @@ class DataPath:
     ar_buff = None
     reg_A_buff = None
 
+    controlUnit: ControlUnit = None
 
-    def __init__(self, stack_size, data_memory_size):
+    def __init__(self, stack_size, data_memory: List[int], controlUnit: ControlUnit = None):
 
         self.stack_size = stack_size
-        self.data_memory_size = data_memory_size
+        self.data_memory_size = len(data_memory)
+        self.controlUnit = controlUnit
 
         self.stack = [0] * self.stack_size
-        self.data_memory = [0] * self.data_memory_size
+        self.data_memory = data_memory
 
         self.sp = -1
         self.ar = 0
@@ -124,18 +120,24 @@ class DataPath:
 
     def signal_latch_SP(self, sel: Mux_Signal):
 
-        assert sel in {Mux_Signal.SEL_SP_NEXT, Mux_Signal.SEL_SP_PREV}, f"internal error, latch_SP incorrect selector: {sel}"
+        assert sel in {
+            Mux_Signal.SEL_SP_NEXT,
+            Mux_Signal.SEL_SP_PREV
+        }, f"internal error, latch_SP incorrect selector: {sel}"
 
         if sel == Mux_Signal.SEL_SP_NEXT:
-            self.sp_buff += 1
+            self.sp_buff = self.sp + 1
         elif sel == Mux_Signal.SEL_SP_PREV:
-            self.sp_buff -= 1
+            self.sp_buff = self.sp - 1
 
-        assert 0 <= self.sp_buff
+        assert 0 <= self.sp_buff < len(self.stack), f"index out of range stack: {self.sp_buff}"
 
     def signal_latch_S(self, sel: Mux_Signal):
 
-        assert sel in {Mux_Signal.SEL_S_TOP, Mux_Signal.SEL_S_STACK}, f"internal error, latch_S incorrect selector: {sel}"
+        assert sel in {
+            Mux_Signal.SEL_S_TOP,
+            Mux_Signal.SEL_S_STACK
+        }, f"internal error, latch_S incorrect selector: {sel}"
 
         if sel == Mux_Signal.SEL_S_TOP:
             self.second_buff = self.top
@@ -179,7 +181,8 @@ class DataPath:
         if (sel == Mux_Signal.SEL_AR_A):
             self.ar_buff = self.reg_A
         elif (sel == Mux_Signal.SEL_AR_CU):
-            pass
+            self.ar_buff = self.controlUnit.curr_instruction.arg
+            assert self.ar_buff != None, f"wrong selector latch_AR {sel}"
 
     def signal_latch_A(self, sel: Mux_Signal):
 
@@ -225,6 +228,143 @@ class DataPath:
     def __str__(self):
         return f"stack: {self.stack}, sp: {self.sp}, top: {self.top}, second: {self.second}"
 
+class MicroInstr:
+
+    latch = None
+    sel = None
+
+    def __init__(self, latch: Signal, sel: Mux_Signal = None):
+        self.latch = latch
+        self.sel = sel
+
+    def __str__(self):
+        return f"latch: {self.latch}, sel: {self.sel}"
+
+
+class ControlUnit:
+
+    program: List[Instruction] = None
+    pc = None
+    dataPath: DataPath = None
+
+    mpc = None
+    reg_R = None
+    return_stack = None
+
+    _tick = None
+
+    curr_instruction: Instruction = None
+
+    next_micro = MicroInstr(Signal.LATCH_MPC, Mux_Signal.SEL_MPC_NEXT)
+    next_sp = MicroInstr(Signal.LATCH_SP, Mux_Signal.SEL_SP_NEXT)
+    prev_sp = MicroInstr(Signal.LATCH_SP, Mux_Signal.SEL_SP_PREV)
+    s_from_top = MicroInstr(Signal.LATCH_S, Mux_Signal.SEL_S_TOP)
+
+    next_pc = MicroInstr(Signal.LATCH_PC, Mux_Signal.SEL_PC_NEXT)
+    _microprogram = [
+        #(Instructin Fetch)'''
+        #(0)'''
+        [MicroInstr(Signal.LATCH_MPC, Mux_Signal.SEL_MPC_OPCODE)],
+        #(push addr)'''
+        #(1)'''
+        [MicroInstr(Signal.LATCH_AR, Mux_Signal.SEL_AR_CU), next_micro, next_sp],
+        #(2)'''
+        [MicroInstr(Signal.WRITE_ST), s_from_top, MicroInstr(Signal.LATCH_T, Mux_Signal.SEL_T_MEM), next_pc]
+    ]
+
+    def __init__(self, dataPath: DataPath, program: List[Instruction], start):
+        self.dataPath = dataPath
+        self.program = program
+        self.pc = start
+        self._tick = 0
+        self.reg_R = 0
+        self.return_stack = [0] * 100
+        self.mpc = 0
+
+    def signal_latch_R(self, sel: Mux_Signal):
+
+        assert sel in {Mux_Signal.SEL_R_NEXT, Mux_Signal.SEL_R_PREV}, f"internal error, latch_R incorrect selector: {sel}"
+
+        if (sel == Mux_Signal.SEL_R_NEXT):
+            self.reg_R += 1
+        elif (sel == Mux_Signal.SEL_R_PREV):
+            self.reg_R -= 1
+
+        assert 0 <= self.reg_R < len(self.return_stack), f"index out of range returnStack: {self.reg_R}"
+
+    def signal_latch_mPC(self, sel: Mux_Signal):
+
+        assert sel in {
+            Mux_Signal.SEL_MPC_NEXT,
+            Mux_Signal.SEL_MPC_OPCODE,
+            Mux_Signal.SEL_MPC_FETCH
+        }, f"internal error, latch_mPC incorrect selector: {sel}"
+
+        if (sel == Mux_Signal.SEL_MPC_NEXT):
+            self.mpc += 1
+        elif (sel == Mux_Signal.SEL_MPC_FETCH):
+            self.mpc = 0
+        elif (sel == Mux_Signal.SEL_MPC_OPCODE):
+            self.mpc = 1
+            self.curr_instruction = self.program[self.pc]
+
+    def signal_latch_PC(self, sel: Mux_Signal):
+
+        assert sel in {
+            Mux_Signal.SEL_PC_NEXT,
+            Mux_Signal.SEL_PC_RET,
+            Mux_Signal.SEL_PC_ADDR,
+            Mux_Signal.SEL_PC_JN,
+            Mux_Signal.SEL_PC_JZ
+        }, f"internal error, latch_PC incorrect selector: {sel}"
+
+        if (sel == Mux_Signal.SEL_PC_NEXT):
+            self.pc += 1
+        elif (sel == Mux_Signal.SEL_PC_ADDR):
+            self.pc = self.arg
+        elif (sel == Mux_Signal.SEL_PC_JN):
+            self.pc = self.arg if self.dataPath.top < 0 else self.pc + 1
+        elif (sel == Mux_Signal.SEL_PC_JZ):
+            self.pc = self.arg if self.dataPath.top == 0 else self.pc + 1
+        elif (sel == Mux_Signal.SEL_PC_RET):
+
+            assert self.reg_R >= 0, f"return stack is empty"
+
+            self.pc = self.return_stack[self.reg_R]
+
+    def dispatch(self, microinstr: MicroInstr):
+
+        assert microinstr.latch in Signal, f"wrong latch: {microinstr.latch}"
+        sel: Mux_Signal = microinstr.sel
+
+        if (microinstr.latch == Signal.LATCH_PC):
+            self.signal_latch_PC(sel)
+
+        elif (microinstr.latch == Signal.LATCH_R):
+            self.signal_latch_R(sel)
+
+        elif (microinstr.latch == Signal.LATCH_MPC):
+            self.signal_latch_mPC(sel)
+
+        elif (microinstr.latch == Signal.LATCH_AR):
+            self.dataPath.signal_latch_AR(sel)
+
+        elif (microinstr.latch == Signal.LATCH_SP):
+            self.dataPath.signal_latch_SP(sel)
+
+        elif (microinstr.latch == Signal.LATCH_S):
+            self.dataPath.signal_latch_S(sel)
+
+        elif (microinstr.latch == Signal.LATCH_T):
+            self.dataPath.signal_latch_T(sel)
+
+        elif (microinstr.latch == Signal.WRITE_ST):
+            self.dataPath.signal_write_st()
+
+        else:
+            assert False, f"unknown latch: {microinstr.latch}"
+
+
 def main(code_file, mem_file, input_file):
     with open(code_file, 'rb') as f:
         binary_instruction = f.read()
@@ -234,52 +374,23 @@ def main(code_file, mem_file, input_file):
     with open(mem_file, 'rb') as f:
         binary_memory = f.read()
 
-    print(binary_memory)
-    print(from_bytes_data(binary_memory))
-    print(from_bytes_instruction(binary_instruction))
+    data = from_bytes_data(binary_memory)
+    instructions = [Instruction(Opcode.PUSH, arg=16, addr = 0)]
 
-    dp: DataPath = DataPath(10, len(binary_memory))
-    dp.start_cycle()
-    dp.signal_latch_SP(Mux_Signal.SEL_SP_NEXT)
-    dp.update()
-    dp.start_cycle()
-    dp.signal_write_st()
-    dp.signal_latch_S(Mux_Signal.SEL_S_TOP)
-    dp.signal_latch_T(Mux_Signal.SEL_T_IMM)
-    dp.update()
+    dp: DataPath = DataPath(10, data)
+    cu: ControlUnit = ControlUnit(dp, instructions, start= 0)
+    dp.controlUnit = cu
+    code = cu._microprogram
 
+    for tick in code:
+
+        dp.start_cycle()
+        for m in tick:
+            print(m)
+            print(dp)
+            cu.dispatch(m)
+        dp.update()
     print(dp)
-
-    dp.start_cycle()
-    dp.signal_latch_SP(Mux_Signal.SEL_SP_NEXT)
-    dp.update()
-    dp.start_cycle()
-    dp.signal_write_st()
-    dp.signal_latch_S(Mux_Signal.SEL_S_TOP)
-    dp.signal_latch_T(Mux_Signal.SEL_T_IMM)
-    dp.update()
-
-    print(dp)
-
-    dp.start_cycle()
-    dp.signal_latch_SP(Mux_Signal.SEL_SP_NEXT)
-    dp.update()
-    dp.start_cycle()
-    dp.signal_write_st()
-    dp.signal_latch_S(Mux_Signal.SEL_S_TOP)
-    dp.signal_latch_T(Mux_Signal.SEL_T_IMM)
-    dp.update()
-
-    print(dp)
-
-    dp.start_cycle()
-    dp.signal_latch_S(Mux_Signal.SEL_S_TOP)
-    dp.signal_latch_T(Mux_Signal.SEL_T_SECOND)
-    dp.alu(ALU_Signal.ALU_ADD)
-    dp.update()
-    print(dp)
-    print(dp.alu_res)
-
 
 if __name__ == "__main__":
     main("instruction_memory.bin", "data_memory.bin", ".")
